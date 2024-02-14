9ead561af9ffe83854373d04f6f8bbdebe0a2696
hayannn <dlgkdis801@naver.com>
2024-02-14 14:35:39 +0900

    feat: 14강 Templates 프로젝트 구성

--- board/views.py
+++ board/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def board(request):
+    if request.method == "GET":
+        context = {
+            "title":"안녕 베어유?"
+        }
+        return render(request, 'page/index.html', context=context)
