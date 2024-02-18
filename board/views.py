11ef92ba61d7610bb87035df9cd2b7e3a6d6c672
hayannn <dlgkdis801@naver.com>
2024-02-18 18:03:22 +0900

    feat: 18강 백엔드 게시판 리스트 및 페이징 구현

--- board/views.py
+++ board/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from board.models import Post
+from django.core.paginator import Paginator
 
 def board(request):
+    # 게시글 리스트
     if request.method == "GET":
+        page = request.GET.get('page', 1)
+        post_set = Post.objects.all().order_by('-id')
+        paginator = Paginator(post_set, 4)
+
+        post_set = paginator.get_page(page)
+
         context = {
-            "title":"안녕 베어유?"
+            "post_set":post_set
         }
         return render(request, 'page/index.html', context=context)
